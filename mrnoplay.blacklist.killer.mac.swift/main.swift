b9306ddb350fe8eb5016bb640249572f7ba06463
Tianze Ds Qiu <slaaker@126.com>
2020-07-25 13:20:50 +0800

    mac work finished
    - tell between blacklist and whitelist
    - change between Chinese and English
    - forbid app in a elegant way
    - notifications

--- mrnoplay.blacklist.killer.mac.swift/main.swift
+++ mrnoplay.blacklist.killer.mac.swift/main.swift
@@ -7,21 +7,41 @@
 //
 
 import Foundation
+import AppKit
+import os
+import UserNotifications
 
 var bundleIds:[String] = []
 var listType = "black"
 var isBlackList = true
+var languageUsing = "cn"
+var isCn = true
 
-func parseArguments() {
+//
+// About the Arguments Start
+//   arg 1: Language
+//     cn = use Chinese
+//     en = use English (default)
+//   arg 2: List Mode
+//     black = Black List
+//     white = White List (default)
+//   arg 3~n: BundleIds
+//     eg. com.yourcompany.yourapp
+// About the Arguments End
+//
+
+func parseBundleIdsArguments() {
     var arguments = CommandLine.arguments
-    if arguments.count < 2 {
+    if arguments.count < 3 {
         return
     }
-    listType = arguments[1]
+    languageUsing = arguments[1]
+    listType = arguments[2]
+    isCn = languageUsing == "cn"
     isBlackList = listType == "black"
     var arg_i = 0
     for arg in arguments {
-        if arg_i < 2 {
+        if arg_i < 3 {
             arg_i += 1;
         } else {
             bundleIds.append(arg)
@@ -29,4 +49,65 @@ func parseArguments() {
     }
 }
 
-parseArguments()
+parseBundleIdsArguments()
+
+func runInTerminal(_ command: String) -> String {
+    let task = Process()
+    task.launchPath = "/bin/bash"
+    task.arguments = ["-c", command]
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
+    return output
+}
+
+var theNotificationCenter = NSWorkspace.shared.notificationCenter
+
+class AppLaunch: NSObject {
+    override init() {
+        super.init()
+        theNotificationCenter.addObserver(self, selector: #selector(appLaunched(notification:)), name: NSWorkspace.willLaunchApplicationNotification, object: nil)
+    }
+    
+    func showAlert(message:String) {
+        _ = runInTerminal("osascript -e 'tell app \"System Events\" to display dialog \"\(message)\"'")
+    }
+    
+    @objc func appLaunched(notification: Notification) {
+        let userInfo = notification.userInfo
+        let appBundleId = userInfo?["NSApplicationBundleIdentifier"] as! String
+        if(isBlackList) {
+            if(bundleIds.contains(appBundleId)) {
+                let appPId = userInfo?["NSApplicationProcessIdentifier"] as! pid_t
+                let appName = userInfo?["NSApplicationName"] as! String
+                os.kill(appPId, SIGKILL)
+                if(isCn) {
+                    showAlert(message: "\(appName)目前已被禁止使用。")
+                }else{
+                    showAlert(message: "\(appName) is forbidden, you cannot open it now.")
+                }
+            }
+        }else{
+            if(!bundleIds.contains(appBundleId)) {
+                let appPId = userInfo?["NSApplicationProcessIdentifier"] as! pid_t
+                let appName = userInfo?["NSApplicationName"] as! String
+                os.kill(appPId, SIGKILL)
+                if(isCn) {
+                    showAlert(message: "\(appName)目前已被禁止使用。")
+                }else{
+                    showAlert(message: "\(appName) is forbidden, you cannot open it now.")
+                }
+            }
+        }
+    }
+    
+    deinit {
+        theNotificationCenter.removeObserver(self)
+    }
+}
+
+let appLaunch = AppLaunch()
+
+CFRunLoopRun()
